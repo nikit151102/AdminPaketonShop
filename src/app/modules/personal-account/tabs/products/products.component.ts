c788d38a382879f5d18e5bd4ef860bda73775809
nikita <nikit15110295@gmail.com>
2025-09-17 12:03:05 +0700

    ADD. Вкладка товаров

--- src/app/modules/personal-account/tabs/products/products.component.ts
+++ src/app/modules/personal-account/tabs/products/products.component.ts
@@ -1,20 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ProductsService } from './products.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { formatShortDate } from '../../../../../utils/date.utils';
 
 @Component({
   selector: 'app-products',
-  imports: [],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrls: ['./products.component.scss'],
+  imports: [CommonModule, FormsModule]
 })
-export class ProductsComponent implements OnInit{
+export class ProductsComponent implements OnInit {
+  products: any[] = [];
+  loading = false;
+  page = 0;
+  pageSize = 30;
 
-  constructor(private productsService:ProductsService){}
+  columns: { [key: string]: boolean } = {
+    article: true,
+    fullName: true,
+    description: false,
+    retailPrice: true,
+    wholesalePrice: true,
+    createDateTime: true,
+    manufacturer: false,
+    packCount: true,
+    productImageLinks: true,
+    keywords: false
+  };
+
+  columnsVisible = false;
+
+  columnOptions = [
+
+    { key: 'fullName', label: 'Название' },
+    { key: 'description', label: 'Описание' },
+    { key: 'retailPrice', label: 'Розничная цена' },
+    { key: 'wholesalePrice', label: 'Оптовая цена' },
+    { key: 'createDateTime', label: 'Дата создания' },
+    { key: 'manufacturer', label: 'Производитель' },
+    { key: 'packCount', label: 'Кол-во в упаковке' },
+    { key: 'productImageLinks', label: 'Изображение' },
+    { key: 'keywords', label: 'Ключевые слова' }
+  ];
+
+  selectedColumns: string[] = ['article', 'fullName', 'retailPrice', 'wholesalePrice', 'createDateTime', 'packCount', 'productImageLinks'];
+
+  constructor(private productsService: ProductsService) { }
 
   ngOnInit(): void {
-    this.productsService.getProducts([],0,30).subscribe((data:any)=>{
-      console.log('data',data);
-    })
+    this.loadProducts();
+  }
+
+  get filteredSelectedColumns(): string[] {
+    return this.selectedColumns.filter(col => col !== 'article');
+  }
+
+  formatShortDate(date: string) {
+    return formatShortDate(date);
+  }
+  loadProducts(): void {
+    if (this.loading) return;
+    this.loading = true;
+
+    this.productsService.getProducts([], this.page, this.pageSize).subscribe((data: any) => {
+      if (data && data.data) {
+        this.products = [...this.products, ...data.data];
+        this.page++;
+      }
+      this.loading = false;
+    });
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const scrollPosition = window.scrollY + window.innerHeight;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition === documentHeight) {
+      this.loadProducts();
+    }
+  }
+
+  editProduct(productId: string): void {
+  }
+
+  deleteProduct(productId: string): void {
+  }
+
+  toggleColVisibility() {
+    this.columnsVisible = !this.columnsVisible
+  }
+
+  toggleColumnVisibility(columnKey: string, value: boolean): void {
+    this.columns[columnKey] = value;
+    this.selectedColumns = Object.keys(this.columns).filter(key => this.columns[key]);
+  }
+
+  getColumnLabel(columnKey: string): string {
+    const column = this.columnOptions.find(option => option.key === columnKey);
+    return column ? column.label : columnKey;
+  }
+
+  removeColumn(columnKey: string): void {
+    this.selectedColumns = this.selectedColumns.filter(column => column !== columnKey);
+    this.columns[columnKey] = false;
   }
 
 }
+
+
+
+
